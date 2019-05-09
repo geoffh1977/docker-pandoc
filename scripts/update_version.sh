a345debde0aefeec21b6c97151d6eacf3407ac10
Geoffrey Harrison <geoffh1977@gmail.com>
2019-05-09 16:37:34 +1000

    Update Repo Format

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -3,12 +3,12 @@
 
 # Get The Versions Of The Software
 SITE_VERSION=$(curl -s https://github.com/jgm/pandoc/releases/latest | grep -Eo "[0-9]{1,2}\.[0-9]{1,2}")
-LOCAL_VERSION=$(grep "finalImageVersion" container.conf | cut -d= -f 2)
+LOCAL_VERSION=$(grep "finalImageVersion" config.yml | cut -d: -f 2 | sed 's/^ //g')
 
 # Check Versions And Update File
 if [ "$SITE_VERSION" != "$LOCAL_VERSION" ]
 then
-  sed -i "s/^finalImageVersion=.*/finalImageVersion=${SITE_VERSION}/" container.conf
+  sed -i "s/^finalImageVersion:.*/finalImageVersion:\ ${SITE_VERSION}/" config.yml
   echo " Version Updated."
 else
   echo " No Version Change."
